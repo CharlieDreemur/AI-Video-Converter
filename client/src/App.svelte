1c94394d95bd0d2cb8056e87abb9e2c9a982e5cc
Elijah Mock <28277163+ekcom@users.noreply.github.com>
2023-02-25 05:50:32 -0600

    wrap up frontend and document
    ## DOCUMENTATION NOTES:
    - add client-side list of sampling methods and models to the array in client/src/util/InputForm.svelte:13 and :14
    
    - everything should be handled properly on submit
    - the client expects a response from `/dlv?dfn=[VIDEO_UPLOADED_NAME]`
      - to change this, see InputForm.svelte:4
    - for final build, `cd src && npm run build && cd .. && python server.py`
      - navigate to http://127.0.0.1:5000

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -5,10 +5,6 @@
 	import InputForm from "./util/InputForm.svelte";
     import Logo from "./Logo.svelte"
 
-	let clientVideo = null;
-	let serverVideoSrc = clientVideo == null ? null : `/dlv?dfn=${clientVideo.name}`;
-	let fps = 1;
-
 	// probably not the best way to do this
 	let doLogoAnimation = false;
 	let lastMoveTimeout = setTimeout(() => doLogoAnimation = true, 1000);
@@ -25,6 +21,6 @@
 	<Logo anim={doLogoAnimation} />
 	<h1 class="text-2xl text-center mt-4 mb-2">AI Video Convertor</h1>
 	
-	<InputForm bind:fps={fps} bind:videoData={clientVideo} bind:serverVideoSrc={serverVideoSrc} />
+	<InputForm />
 	
 </div>
