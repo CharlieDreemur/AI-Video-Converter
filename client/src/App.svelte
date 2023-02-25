b25f595f77aea3c366686f3be6d9f563e20923be
Elijah Mock <28277163+ekcom@users.noreply.github.com>
2023-02-24 20:19:25 -0600

    work on UI form
    - fail to link VideoPlayer

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,11 +1,51 @@
 <script>
-	let rand = -1;
-	function getRand() {
-	  fetch("./rand")
-		.then(d => d.text())
-		.then(d => (rand = d));
+	// import('https://cdn.tailwindcss.com');
+
+	import VideoPlayer from "src/util/VideoPlayer.svelte";
+
+	let uploadURL = "/api/diffuse"; // todo
+	
+	let clientVideoSrc = null;
+	let serverVideoSrc = null;
+	
+	function clientVideoUpdated(e) {
+		const file = e.target.files[0];
+		clientVideoSrc = URL.createObjectURL(file);
+		clientVideoName = URL.createObjectURL(file.name);
 	}
-  </script>
-  
-  <h1>Your number is {rand}!</h1>
-  <button on:click={getRand}>Get a random number</button>
+
+	function getPromptHint() {
+		// todo look at clientPrompt
+		return "hi";
+	}
+
+	// $:
+	let promptHint = getPromptHint();
+	let clientPrompt = null;
+</script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<h1>AIVideoConvertor</h1>
+<Test />
+<p>
+	Select a file to upload.
+</p>
+
+<form action={uploadURL} method="POST" enctype="multipart/form-data">
+    <input id="video" name="video" type="file" accept="video/*" on:change={clientVideoUpdated} required>
+	<p>{promptHint}</p>
+    <input id="prompt" name="prompt" type="text" bind:value={clientPrompt} required>
+    <input type="submit" class="button" value="Upload">
+</form>
+
+<div class="grid-cols-2">
+	<div>
+		<p>Input</p>
+		<p>{promptHint}</p>
+		<VideoPlayer srcURL={clientVideoSrc} />
+	</div>
+	<div>
+		<p>Output</p>
+		<VideoPlayer srcURL={serverVideoSrc} />
+	</div>
+</div>
---
+++ client/src/util/VideoPlayer.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let srcURL;
+	// export let clientVideoWidth;
+	// export let clientVideoHeight;
+    // width={clientVideoHeight} height={clientVideoWidth}
+    // bind:clientWidth={clientVideoWidth} bind:clientHeight={clientVideoHeight}
+</script>
+
+<div>
+    Input
+    {#if srcURL != null}
+    <video controls>
+        <source src={srcURL} type="video/{srcURL.slice(-srcURL.lastIndexOf('.'))}">
+        Can't preview: Your browser doesn't support the video tag.
+    </video>
+    <i class="fa fa-trash"></i>
+    {/if}
+</div>
