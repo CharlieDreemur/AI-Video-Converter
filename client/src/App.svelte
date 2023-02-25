ba4a8d3eeba2ab8e891d4073ff54499d457dbf66
Elijah Mock <28277163+ekcom@users.noreply.github.com>
2023-02-24 23:25:43 -0600

    improve UI and make form submit it background
    - refactor App.svelte
    - use tailwindcss
    - create form submission in background helper function

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,78 +1,30 @@
+<svelte:head>
+	<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+</svelte:head>
 <script>
 	import VideoPlayer from "./util/VideoPlayer.svelte";
+	import InputForm from "./util/InputForm.svelte";
 
-	let uploadURL = "/api/diffuse";
-	
-	const samplingMethods = ["Euler a", "idk"]; // todo add
-	const models = ["protogenX34OfficialR_1.ckpt"]; // todo add
-	
-	let clientVideoSrc = null;
-	let serverVideoSrc = null;
-	
-	function clientVideoUpdated(e) {
-		const file = e.target.files[0];
-		console.log(file);
-		if (e == undefined) {
-			clientVideoSrc = null;
-			clientVideoName = null;
-		} else {
-			//el.srcObject = file
-			clientVideoSrc = window.URL.createObjectURL(file);
-			clientVideoName = window.URL.createObjectURL(file.name);
-		}
-	}
+	let clientVideoSrc = null; // todo not working
+	let serverVideoSrc = null; // todo get from form response
 
-	let clientPrompt = null;
-	
-	//$: promptHint = clientPrompt.length < 8 ? "Add more details" : null;
-
-	const promptPlaceholder = "Make it spicy"; // todo generate random prompt
+	let serverResponse = null;
+	// todo handle changes to serverResponse
+	// parse JSON and update serverVideoSrc
 </script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<h1>AI Video Convertor</h1>
+<h1 class="text-2xl text-center mt-6 mb-2">AI Video Convertor</h1>
 
-<form action={uploadURL} method="POST" enctype="multipart/form-data">
-	<label for="video">
-		<span>Input video</span>
-		<input id="video" name="video" type="file" accept="video/*" on:change={clientVideoUpdated} required />
-	</label>
-	<!--<span>{promptHint}</span>-->
-	<label for="prompt">
-		<span>Prompt</span>
-		<input id="prompt" name="prompt" type="text" bind:value={clientPrompt} placeholder={promptPlaceholder} />
-	</label>
-	<label for="model">
-		<span>Model</span>
-		<select>
-			{#each models as op}
-				<option value={op}>
-					{op}
-				</option>
-			{/each}
-		</select>
-	</label>
-	<label>
-		<span>Sampling method</span>
-		<select>
-			{#each samplingMethods as op}
-				<option value={op}>
-					{op}
-				</option>
-			{/each}
-		</select>
-	</label>
+<InputForm bind:videoSrc={clientVideoSrc} bind:responseText={serverResponse} />
 
-    <input type="submit" value="Upload">
-</form>
-
-<div class="grid-cols-2">
+<div class="grid sm:grid-cols-2 justify-center text-center">
 	<div>
 		<p>Input</p>
-		<VideoPlayer srcURL={clientVideoSrc} />
+		<VideoPlayer bind:srcURL={clientVideoSrc} />
 	</div>
 	<div>
 		<p>Output</p>
-		<VideoPlayer srcURL={serverVideoSrc} />
+		<VideoPlayer bind:srcURL={serverVideoSrc} />
 	</div>
 </div>
