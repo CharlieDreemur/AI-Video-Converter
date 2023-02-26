55f4129535513c1d203e432c8ea284886b8941a4
CharlieDreemur <zhangwj.charlie@gmail.com>
2023-02-25 22:08:37 -0600

    implement frameconverter

--- frameconverter.py
+++ frameconverter.py
@@ -17,7 +17,61 @@ def format_timedelta(td):
     ms = round(ms / 1e4)
     
     return f"{result}.{ms:02}".replace(":", "-")
-def convert_frames_to_video(pathIn,pathOut,fps):
+def get_saving_frames_durations(cap, saving_fps):
+    """A function that returns the list of durations where to save the frames"""
+    s = []
+    # get the clip duration by dividing number of frames by the number of frames per second
+    clip_duration = cap.get(cv2.CAP_PROP_FRAME_COUNT) / cap.get(cv2.CAP_PROP_FPS)
+    # use np.arange() to make floating-point steps
+    for i in np.arange(0, clip_duration, 1 / saving_fps):
+        s.append(i)
+    return s
+def video2frame(video_file,framepersec=60):
+    SAVING_FRAMES_PER_SECOND = framepersec
+    filename, _ = os.path.splitext(video_file)
+    filename += "-opencv"
+    # make a folder by the name of the video file
+    if not os.path.isdir(filename):
+        os.mkdir(filename)
+    # read the video file    
+    cap = cv2.VideoCapture(video_file)
+    # get the FPS of the video
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    # if the SAVING_FRAMES_PER_SECOND is above video FPS, then set it to FPS (as maximum)
+    saving_frames_per_second = min(fps, SAVING_FRAMES_PER_SECOND)
+    # get the list of duration spots to save
+    saving_frames_durations = get_saving_frames_durations(cap, saving_frames_per_second)
+    # start the loop
+    count = 0
+    temp=0
+    while True:
+        is_read, frame = cap.read()
+        if not is_read:
+            # break out of the loop if there are no frames to read
+            break
+        # get the duration by dividing the frame count by the FPS
+        frame_duration = count / fps
+        try:
+            # get the earliest duration to save
+            closest_duration = saving_frames_durations[0]
+        except IndexError:
+            # the list is empty, all duration frames were saved
+            break
+        if frame_duration >= closest_duration:
+            # if closest duration is less than or equals the frame duration, 
+            # then save the frame
+            frame_duration_formatted = format_timedelta(timedelta(seconds=frame_duration))
+            cv2.imwrite(os.path.join(filename, f"frame{temp}.png"), frame) 
+            temp=temp+1
+            # drop the duration spot from the list, since this duration spot is already saved
+            try:
+                saving_frames_durations.pop(0)
+            except IndexError:
+                pass
+        # increment the frame count
+        count += 1
+
+def frame2video(pathIn,pathOut,fps):
     frame_array = []
     files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f))]
     #for sorting the file names properly
@@ -37,7 +91,7 @@ def convert_frames_to_video(pathIn,pathOut,fps):
         out.write(frame_array[i])
     out.release()
     
-def outputfolder(pathIn, pathOut):
+def processframes(pathIn, pathOut):
     foldername = "D:\StudyLife\Github\HackIllinois\output"
     path=os.path.join(pathIn,foldername)
     if not os.path.isdir(path):
@@ -56,11 +110,7 @@ def outputfolder(pathIn, pathOut):
         webuiAPI.client.saveimg(path=pathOut, img=output, fileName=f"frame{temp}")
         temp+=1
         
-def main(pathIn,fps):
-    pathOut = 'video.avi'
-    convert_frames_to_video(pathIn, pathOut, fps)
 if __name__=="__main__":
     import sys
     pathin=sys.argv[1]
-    framepersec=float(sys.argv[2])
-    main(pathin,framepersec)
+    pathout=sys.argv[2]
