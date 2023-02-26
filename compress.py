968fbe999591bb7730a493ed4cdf5a2514ca5c9d
yuqingg3 <123529949+yuqingg3@users.noreply.github.com>
2023-02-26 00:54:38 -0600

    Create compress.py
    compress(input_path)

---
+++ compress.py
@@ -0,0 +1,8 @@
+def resize_png(image_path):
+    with Image.open(image_path) as im:
+        width, height = im.size
+        if width > 800 or height > 800:
+            ratio = min(800/width, 800/height)
+            new_size = (int(width*ratio), int(height*ratio))
+            im = im.resize(new_size)
+            im.save(image_path)
